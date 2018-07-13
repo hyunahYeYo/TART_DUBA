1a8f67b5eaecd6017825a6228ee98695e886af50
RosieInThePark <325rabbit@gmail.com>
2018-07-14 06:51:31 +0900

    Loading Page

---
+++ app/src/main/java/sjsu/tart/duba/LoadingActivity.java
@@ -0,0 +1,27 @@
+package sjsu.tart.duba;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by RosieHyunahPark on 2018-07-14.
+ */
+
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
--- app/src/main/java/sjsu/tart/duba/MainActivity.java
+++ app/src/main/java/sjsu/tart/duba/MainActivity.java
@@ -1,5 +1,6 @@
 package sjsu.tart.duba;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,6 +21,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Loading Page
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
