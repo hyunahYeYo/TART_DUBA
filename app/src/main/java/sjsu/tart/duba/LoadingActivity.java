997bd5e63ed436514baa8a39b90e5964ccbb2a4c
haaaak <41180594+haaaak@users.noreply.github.com>
2018-07-17 12:13:00 +0900

    ㅇㅇ

--- app/src/main/java/sjsu/tart/duba/LoadingActivity.java
+++ app/src/main/java/sjsu/tart/duba/LoadingActivity.java
@@ -10,7 +10,7 @@ import android.os.Handler;
 
 public class LoadingActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+      protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         startLoading();
--- app/src/main/java/sjsu/tart/duba/MainActivity.java
+++ app/src/main/java/sjsu/tart/duba/MainActivity.java
@@ -92,13 +92,8 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
